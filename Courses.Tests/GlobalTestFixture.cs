b214fde9a40d008661ae2ab86f50a5f6eb1c263e
Karol Moroz <karol@moroz.dev>
2025-10-08 16:08:59 +0800

    Update tests based on blog post

--- Courses.Tests/GlobalTestFixture.cs
+++ Courses.Tests/GlobalTestFixture.cs
@@ -1,22 +1,34 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Courses.Tests;
 
 public class GlobalTestFixture : IAsyncLifetime
 {
-    private CustomWebApplicationFactory? _factory;
+    private WebApplicationFactory<Program>? _factory;
 
-    public CustomWebApplicationFactory Factory =>
+    public WebApplicationFactory<Program> Factory =>
         _factory ?? throw new InvalidOperationException("Factory is not initialized");
+
+    public AsyncServiceScope AsyncScope => Factory.Services.CreateAsyncScope();
 
     public async Task InitializeAsync()
     {
-        _factory = new CustomWebApplicationFactory();
-        await _factory.Migrate();
+        _factory = new WebApplicationFactory<Program>().WithWebHostBuilder(builder => builder.UseEnvironment("Test"));
+        await MigrateDb();
     }
 
     public Task DisposeAsync()
     {
         return Task.CompletedTask;
+    }
+
+    private async Task MigrateDb()
+    {
+        await using var scope = AsyncScope;
+        var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+        await db.Database.MigrateAsync();
     }
 }
--- Courses.Tests/UnitTest1.cs
+++ Courses.Tests/UnitTest1.cs
@@ -1,4 +1,8 @@
 ﻿using System.Net;
+using Courses.Grpc;
+using Courses.Repository;
+using Grpc.Net.Client;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace Courses.Tests;
 
@@ -13,10 +17,29 @@ public class UnitTest1(GlobalTestFixture fixture) : DbTestBase(fixture)
     }
 
     [Fact]
-    public async Task Test_Setup()
+    public async Task Test_EventRepository()
+    {
+        await using var scope = Fixture.AsyncScope;
+        var repo = scope.ServiceProvider.GetRequiredService<IEventRepository>();
+        var actual = await repo.ListEvents();
+        Assert.Empty(actual);
+    }
+
+    [Fact]
+    public async Task Test_HttpClient()
     {
         var client = Fixture.Factory.CreateDefaultClient();
         var response = await client.GetAsync("/");
         Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+    }
+
+    [Fact]
+    public async Task Test_GrpcClient()
+    {
+        var client = Fixture.Factory.CreateDefaultClient();
+        var channel = GrpcChannel.ForAddress(client.BaseAddress!, new GrpcChannelOptions { HttpClient = client, });
+        var grpcClient = new CoursesApi.CoursesApiClient(channel);
+        var actual = await grpcClient.ListEventsAsync(new ListEventsRequest());
+        Assert.Empty(actual.Events);
     }
 }
